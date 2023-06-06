bd965368731eb6d3c58d828f5ddaf0816730dcee
Miguel Chavez <toonchavez8@gmail.com>
2023-06-05 20:11:45 -0600

    this commit will start taliwnd react with daisy ui

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+	return (
+		<>
+			<button className="btn btn-primary">One</button>
+			<button className="btn btn-secondary">Two</button>
+			<button className="btn btn-accent btn-outline">Three</button>
+		</>
+	);
+}
+
+export default App;
